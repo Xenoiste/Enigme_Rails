31e8df6c4e54505e7be152b29515a3012f382a7f
Xenoiste <theo.lavauzelle@gmail.com>
2022-12-15 14:14:41 +0100

    Ajout de l'enigme CSS avec pour but de faire apparâitre le bouton suivant

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
-
+    @avoirBon
+    if params.has_key? :reponse and not params[:reponse].blank?
+      @reponse = params[:reponse]
+      if params[:reponse] == "visibility: visible;"
+        @avoirBon = true
+      else
+        @avoirBon = false
+      end
+    end
   end
 end
